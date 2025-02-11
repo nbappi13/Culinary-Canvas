bca7a10022a3a20595daa76282caaaa56fab85ce
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-02-11 15:38:22 +0600

    created some folders: home, router, layout, shared with some files and rendered that

---
+++ src/router/router.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route Not Found!</h2>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
+      
+    },
+  ]);
+
+export default router;
